8bc1e8315fca4b926578fd569e0d983fd0600773
David Millman <dave@cs.unc.edu>
2015-10-01 00:29:00 -0400

    Add inital version of feed generation from json
    This is only an initial version because it is written procedurally,
    it folows my data munging from python.

--- to_rss.js
+++ to_rss.js
@@ -7,67 +7,79 @@ var Fs = require('fs');
 var Rss = require('rss');
 
 // global constans
-var URL = 'https://muut.com/comptop';
+var SITE_URL = 'https://muut.com/comptop';
+var FEED_URL = 'https://muut.com/comptop';
+var SESSION_ID = 'vkx7sqKjKuJaZ4fxk5BxWqWz.50ae54e856e5d118bb16b4b3fdaf0b28b7686000';
+var MAINTAINER_EMAIL = 'dave@cs.unc.edu';
 
 // the data to get things going
 var fileName = 'data/data-2015-09-18.json';
 var extracted_data = {};
 
-function makeData(author, description, postTitle, url, guid, date) {
+function makeEntryData(author, description, postTitle, url, guid, date) {
     return {
-        'author': author,
-        'description': description,
         'title': postTitle,
+        'description': description,
         'url': url,
         'guid': guid,
+        'author': author,
         'date': date
     };
 }
 
-function extract(entry) {
-    // all the values below must be set for the entry
-    var author, description, postTitle, guid, date;
-    try {
-        seed = entry.seed;
-        author = seed.user.displayname;
-        description = seed.body;
-        postTitle = seed.title;
-        guid = seed.key;
-        date = seed.time;
-    } catch (error){
-        author = 'ERROR';
-        description = 'There was an error extracting data from an entry. ' +
-            'Please contact dave@cs.unc.edu to report an error.';
-        postTitle = 'Errror extracting data for an entry';
-        guid = new Date().getTime();
-        date = guid;
-    }
-    var data = makeData(author, description, postTitle, URL, guid, date);
-    console.log("data: ", data);
-    return data;
+function makeEntryErrorData() {
+    author = 'ERROR';
+    description = 'There was an error extracting data from an entry. ' +
+        'Please contact ' + MAINTAINER_EMAIL + ' to report an error.';
+    postTitle = 'Error extracting data for an entry';
+    guid = new Date().getTime();
+    date = guid;
+    return makeEntryData(author, description, postTitle, guid, date);
 }
 
-function createFeed(data) {
-    feed = new Rss({
-        'title': 'https://muut.com/comptop#!/',
-        'description': 'rss feed of the computational topology message board',
-        'feed_url': 'https://TODO!!',
-        'site_url': 'https://muut.com/comptop#!/'
+function extract(entry) {
+    var entryData;
+    try {
+        var seed = entry.seed;
+        var author = seed.user.displayname;
+        var description = seed.body;
+        var postTitle = seed.title;
+        var guid = seed.key;
+        var date = seed.time;
+        entryData = makeEntryData(author, description, postTitle,
+                                  SITE_URL, guid, date);
+    } catch (error) {
+        entryData = makeEntryErrorData();
+    }
+    return entryData;
+}
 
+function createFeed(entries) {
+    feed = new Rss({
+        'title': 'Computational Topology Message Board',
+        'description': 'RSS feed of the computational topology message board',
+        'feed_url': FEED_URL,
+        'site_url': SITE_URL
     });
 
-    for (var idx in data) {
-        feed.item(data[idx]);
+    for (var idx in entries) {
+        feed.item(entries[idx]);
     }
 
-    var xml = feed.xml();
-    //console.log(xml)
+    var xml = feed.xml({indent: true});
+    return xml;
 }
 
 function parseData(data) {
-    var entries = data.result.moots.entries;
-    extractedEntries = entries.map(extract);
-    createFeed(extractedEntries);
+    var extractedEntries;
+    try {
+        var entries = data.result.moots.entries;
+        extractedEntries = entries.map(extract);
+    } catch (error) {
+        extractedEntries = [makeEntryErrorData()];
+    }
+    xml = createFeed(extractedEntries);
+    console.log(xml);
 }
 
 // read file
@@ -75,5 +87,6 @@ Fs.readFile(fileName, 'utf8', function(err, data) {
     if (err) { throw err; }
     else {
         var jsonData = JSON.parse(data);
-        parseData(jsonData); }
+        parseData(jsonData);
+    }
 });
